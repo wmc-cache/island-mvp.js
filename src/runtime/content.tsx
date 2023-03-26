8a4c21035f76f376e6621076e12e4084b66da62c
吴梦成 <wumengcheng@192.168.124.6>
2023-03-27 00:32:27 +0800

    add router 2

--- src/runtime/content.tsx
+++ src/runtime/content.tsx
@@ -1,24 +1,26 @@
 import { useRoutes } from 'react-router-dom';
-import A from '../../docs/guide/a';
-import B from '../../docs/guide/b';
-import Index from '../../docs/guide/index';
+// import A from '../../docs/guide/a';
+// import B from '../../docs/guide/b';
+// import Index from '../../docs/guide/index';
+import { routes } from 'island:routes';
 
-const routes = [
-    {
-        path: '/guide',
-        element: <Index />
-    },
-    {
-        path: '/guide/a',
-        element: <A />
-    },
-    {
-        path: '/guide/b',
-        element: <B />
-    }
-];
+// const routes = [
+//     {
+//         path: '/guide',
+//         element: <Index />
+//     },
+//     {
+//         path: '/guide/a',
+//         element: <A />
+//     },
+//     {
+//         path: '/guide/b',
+//         element: <B />
+//     }
+// ];
 
 export const Content = () => {
+    console.log("routes", routes)
     const routeElement = useRoutes(routes);
     return routeElement;
 };
--- src/theme-default/Layout/index.tsx
+++ src/theme-default/Layout/index.tsx
@@ -2,8 +2,8 @@ import { Content } from '../../runtime/Content';
 export function Layout() {
   return (
     <div>
-      <h1>Common Content</h1>
-      <h1>Doc Content</h1>
+      {/* <h1>Common Content</h1>
+      <h1>Doc Content</h1> */}
       <Content />
     </div>
   );
